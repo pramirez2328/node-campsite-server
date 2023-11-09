1ae4985f416fca90f11d557e5e1ea6b27e0cb01e
ramirez <prlara2000@gmail.com>
2023-11-08 19:32:53 -0600

    add authentication

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 var app = express();
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -45,47 +47,26 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
-  console.log('cookie session:', req.session);
-  console.log('session user', req.session.user);
+  console.log(req.user);
 
-  if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === 'admin' && pass === 'password') {
-      req.session.user = 'admin';
-      return next(); // authorized
-    } else {
-      const err = new Error('Enter the correct credentials');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
+  if (!req.user) {
+    const err = new Error('You are not authenticated!, try again');
+    err.status = 401;
+    return next(err);
   } else {
-    if (req.session.user === 'admin') {
-      return next();
-    } else {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-    }
+    return next();
   }
 }
 
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
+const passport = require('passport');
+const User = require('../models/user');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.render('users', { title: 'Users' });
+});
+
+router.post('/signup', (req, res) => {
+  User.register(new User({ username: req.body.username }), req.body.password, (err) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({ err: err });
+    } else {
+      passport.authenticate('local')(req, res, () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ success: true, status: 'Registration Successful!' });
+      });
+    }
+  });
+});
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ success: true, status: 'You are successfully logged in!' });
 });
 
 module.exports = router;
