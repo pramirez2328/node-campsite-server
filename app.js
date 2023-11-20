b3ac270df6bb8a3f0a0f637b6e33a785472624e8
Pedro Ramirez <prlara2000@gmail.com>
2023-11-19 22:54:03 -0600

    finally finish

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 const config = require('./config');
 const mongoose = require('mongoose');
 var express = require('express');
@@ -83,6 +84,7 @@ app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
